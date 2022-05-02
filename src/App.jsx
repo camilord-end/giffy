4a51f6f00d97c43220333929b4b9a00bd103015d
camilord-end <juliancaor10@gmail.com>
2022-05-02 11:29:52 -0500

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <section className="App-content">
+        
+      </section>
+    </div>
+  );
+};
